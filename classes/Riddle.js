ade64e7b7cef0897d34e98c0440952d79c532d3e
hershy rozenfeld <Y6764057@gmail.com>
2025-06-26 16:54:21 +0300

    added proprtyes and methods to riddle

--- classes/Riddle.js
+++ classes/Riddle.js
@@ -1,1 +1,17 @@
+import readLine from 'readline-sync';
 
+export default function Riddle(id, taskDescription, correctAnswer){
+    this.id = id;
+    this.name = "";
+    this.taskDescription = taskDescription;
+    this.correctAnswer = correctAnswer;
+    this.ask = function(){
+        while(!this.check(readLine.question(this.taskDescription))){
+            console.log("Your answer is incorrect");
+        }
+        console.log("Your answer is correct!");
+    }
+    this.check = function(num){
+        return num == this.correctAnswer ? true : false;
+    }
+}
