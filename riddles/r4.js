aac9fec3c9e8af2e7ccbc1efb5fb356fb2b0b2f2
hershy rozenfeld <Y6764057@gmail.com>
2025-06-29 15:51:14 +0300

    added levels And Modul

--- riddles/r4.js
+++ riddles/r4.js
@@ -1,6 +1,8 @@
 export default {
     id: 4,
+    num1:0,
+    num2:0,
     name: "Quick Division",
-    taskDescription: `What is ${12} / ${3}?`,
-    correctAnswer: "4"
+    taskDescription: `What is ${num1} / ${num2}?`,
+    correctAnswer: num1 / num2
 };
