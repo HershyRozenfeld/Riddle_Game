cef34d91ea9e4c778838bed96f94d7247b39e9ea
hershy rozenfeld <Y6764057@gmail.com>
2025-07-02 15:53:04 +0300

    added change of the CR

---
+++ utils/exportToApp.js
@@ -0,0 +1,15 @@
+// Classes
+export { default as Player } from "../classes/Player.js";
+export { default as RiddleManager } from "../classes/RiddleManager.js";
+export { default as UIManager } from "../classes/UIManager.js";
+export { RiddleGame } from "../classes/manageGame.js";
+
+// Riddles
+export { default as allRiddles } from "../riddles/exportRiddles.js";
+
+// Utils
+export { getSumAndAverage, divMinutesAndSeconds } from "./timeUtils.js";
+export { askLevel } from "./gameUtils.js";
+
+// External dependencies
+export { default as readlineSync } from "readline-sync";
