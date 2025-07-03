605bd5e0eb18c5bb495411bc28abcd7be4b17e08
hershy rozenfeld <Y6764057@gmail.com>
2025-07-03 22:24:48 +0300

    fix: Remove create.js file and its associated riddle creation logic

--- DAL/create.js
+++
@@ -1,8 +0,0 @@
-import readlineSync from 'readline-sync';
-import
-function createRiddle(){
-    const level = readlineSync.question('What level you Want? (Easy(1) Medium(2) Hard(3)): ');
-    const riddleJson = readlineSync.question(`Enter riddle JSON (e.g., {"id": 1, "name": "Easy Math 1", "TaskDescription":"What is 45 + 4?", "CorrectAnswer": 49 })`)
-    const data = fileReader(Path ,level)
-    // באמצע עבודה!!!
-}
